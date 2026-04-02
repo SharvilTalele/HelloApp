c250f1bbf4fe66ca767f39720f2e438405e144a1
Sharvil Talele <sharviltalele12@gmail.com>
2026-04-02 08:29:16 +0530

    uc7

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,18 @@
-package com.example;
+public class App {
+    public static void main(String[] args) {
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+        String message;
+
+        // Check if arguments are provided
+        if (args.length == 0) {
+            message = "Hello, World!";
+        } else {
+            // Join names using String.join()
+            String names = String.join(", ", args);
+            message = "Hello, " + names + "!";
+        }
+
+        // Print output
+        System.out.println(message);
     }
 }
